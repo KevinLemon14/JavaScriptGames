150f50ec47a9d5bed8d7446fdf2a4a88ba75e089
Kevin Lemon <kevinseanlemon@gmail.com>
2022-06-27 20:41:16 +0100

    started memory game

---
+++ MemoryGame/app.js
@@ -0,0 +1,54 @@
+const cardArray = [
+    {
+        name: 'fries',
+        img: 'images/fries.png',
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png',
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png',
+    },
+    {
+        name: 'icecream',
+        img: 'images/icecream.png',
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png',
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png',
+    },
+    {
+        name: 'fries',
+        img: 'images/fries.png',
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png',
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png',
+    },
+    {
+        name: 'icecream',
+        img: 'images/icecream.png',
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png',
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png',
+    },
+]
+
+cardArray.sort(() => 0.5 - Math.random())
+
+console.log(cardArray)
